ab343dc1119944d2966ca45f74408c35bdf05db0
XYeam <rs.xuyao@gmail.com>
2020-12-29 11:48:55 +0800

    init background

---
+++ ssh-background/src/main/java/com/x/ssh/WebSshApplication.java
@@ -0,0 +1,21 @@
+package com.x.ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ProjectName: webSsh
+ * @Package: com.x.ssh
+ * @ClassName: WebSshApplication
+ * @Author: Yao
+ * @Description: starter
+ * @CreateDate: 2020/12/29 11:44
+ * @Version:
+ */
+@SpringBootApplication
+public class WebSshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSshApplication.class, args);
+    }
+}
